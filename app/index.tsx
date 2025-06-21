df1bfadf6ac45d97e7a772882b2e1ceb848ce51d
thomasranieri <9074683+thomasranieri@users.noreply.github.com>
2025-06-21 20:19:47 +0930

    Added setup screen, with link from homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>cert-trainer</Text>
+      <Link href="/setup/">
+        <Text style={{ color: "blue" }}>Go to Setup</Text>
+      </Link>
     </View>
   );
 }
---
+++ app/setup/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Setup() {
+  return (
+    <View>
+      <Text>Setup Screen</Text>
+    </View>
+  );
+}
